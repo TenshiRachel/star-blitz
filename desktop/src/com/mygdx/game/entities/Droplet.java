9fcc09b67414d0eb875b6c8bf10a560995e424d8
icelightningmage <144021293+icelightningmage@users.noreply.github.com>
2024-02-16 14:55:29 +0800

    updated droplet.java

--- desktop/src/com/mygdx/game/entities/Droplet.java
+++ desktop/src/com/mygdx/game/entities/Droplet.java
@@ -6,75 +6,30 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 
 public class Droplet extends Entity{
-	private Texture npcTexture;
-	private NPCState state; //enum for different states
-	private NPCAttributes attributes; //attributes for health, strength, etc
+	private Texture dropletTexture;
 	
 	public Droplet(Vector2 pos, float speed, float width, float height) {
 		super(pos.x, pos.y, speed, width, height);
-		this.npcTexture = new Texture(Gdx.files.internal("entities/droplet.png"));
-		this.state = NPCState.IDLE;
-		this.attributes = new NPCAttributes(100,10);
+		this.dropletTexture = new Texture(Gdx.files.internal("entities/droplet.png"));
 	}
-	
-	public enum NPCState {
-	    IDLE,
-	    WALKING,
-	    ATTACKING,
-	    RUNNING,
-	    FLYING
-	}
-	
-	private class NPCAttributes{
-        private int health, strength;
-        
-        public NPCAttributes(int health, int strength) {
-            this.health = health;
-            this.strength = strength;
-        }
-        
-        public int getHealth() {
-            return health;
-        }
-
-        public void setHealth(int health) {
-            this.health = health;
-        }
-        
-        public int getStrength() {
-            return strength;
-        }
-
-        public void setStrength(int strength) {
-            this.strength = strength;
-        }
-    }
-	
+		
 	public void update(float deltaTime) {
 		
 	}
 	
 	public void render(SpriteBatch batch) {
-		batch.draw(npcTexture, getX(), getY(), getWidth(), getHeight());
+		batch.draw(dropletTexture, getX(), getY(), getWidth(), getHeight());
 	}
 	
-	public NPCState getState() {
-		return state;
-	}
-	
-	public void setState(NPCState state) {
-        this.state = state;
-    }
-
 	public Texture getNPC() {
-		return npcTexture;
+		return dropletTexture;
 	}
 
 	public void setNPC(Texture nPC) {
-		npcTexture = nPC;
+		dropletTexture = nPC;
 	}
 	
 	public void dispose() {
-        npcTexture.dispose();
+        dropletTexture.dispose();
     }
 }
